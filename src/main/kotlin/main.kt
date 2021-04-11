414ef63da1d398861d3d295444d818c09083f54c
Javiermnoeda <jmnoeda2000@gmail.com>
2021-04-11 14:31:11 +0200

    Cambio Nombre Variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,15 +9,15 @@ fun main() {
     val lista1 = listOf<Int>(1, 2, 3, 4, 5, 6)
     val lista2 = listOf<Int>(1, 2, 3, 4, 5, 6)
 
-    var x = 0
-    var y = 0
+    var totalJ1 = 0
+    var totalJ2 = 0
 
     runBlocking {
         this.launch {
             repeat(10) {
                 println("\nTirada ${it + 1} de J1, ha sacado: ${lista1.random()}")
                 delay(1000)
-                x += lista1.random()
+                totalJ1 += lista1.random()
             }
             println("\nJ1 ha terminado")
         }
@@ -27,14 +27,14 @@ fun main() {
             repeat(10) {
                 println("\nTirada ${it + 1} de J2, ha sacado: ${lista2.random()}")
                 delay(1500)
-                y += lista2.random()
+                totalJ2 += lista2.random()
             }
             println("\nJ2 ha terminado")
         }
     }
 
-    println("\nJ1 ha sacado $x en total")
-    println("J2 ha sacado $y en total")
+    println("\nJ1 ha sacado $totalJ1 en total")
+    println("J2 ha sacado $totalJ2 en total")
 
-    if (x > y) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
+    if (totalJ1 > totalJ2) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
 }
