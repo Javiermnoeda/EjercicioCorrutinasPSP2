09d950fd8422f5df4d219745ad5727a93251e201
Javiermnoeda <jmnoeda2000@gmail.com>
2021-04-11 14:20:51 +0200

    it +1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,16 +6,16 @@ fun main() {
 
     //Creamos las dos listas
 
-    val lista1 = listOf<Int>(1,2,3,4,5,6)
-    val lista2 = listOf<Int>(1,2,3,4,5,6)
+    val lista1 = listOf<Int>(1, 2, 3, 4, 5, 6)
+    val lista2 = listOf<Int>(1, 2, 3, 4, 5, 6)
 
     var x = 0
     var y = 0
 
     runBlocking {
-        this.launch{
-            repeat(10){
-                println("\nTirada $it J1, ha sacado: ${lista1.random()}")
+        this.launch {
+            repeat(10) {
+                println("\nTirada ${it + 1} de  J1, ha sacado: ${lista1.random()}")
                 delay(1000)
                 x += lista1.random()
 
@@ -24,9 +24,9 @@ fun main() {
         }
 
 
-        this.launch{
-            repeat(10){
-                println("\nTirada $it J2, ha sacado: ${lista2.random()}")
+        this.launch {
+            repeat(10) {
+                println("\nTirada ${it + 1} de J2, ha sacado: ${lista2.random()}")
                 delay(1500)
                 y += lista2.random()
 
@@ -39,5 +39,5 @@ fun main() {
     println("\nJ1 ha sacado $x en total")
     println("J2 ha sacado $y en total")
 
-    if (x>y) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
+    if (x > y) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
 }
