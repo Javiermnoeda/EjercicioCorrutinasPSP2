95b91b296cae8ea852cb2421561378032a461f92
Javiermnoeda <jmnoeda2000@gmail.com>
2021-04-11 14:09:56 +0200

    Identacion y If

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -15,35 +15,29 @@ fun main() {
     runBlocking {
         this.launch{
             repeat(10){
-                println("\n Tirada $it J1, ha sacado: ${lista1.random()}")
+                println("\nTirada $it J1, ha sacado: ${lista1.random()}")
                 delay(1000)
                 x += lista1.random()
 
             }
-            println("\n J1 ha terminado")
+            println("\nJ1 ha terminado")
         }
 
 
         this.launch{
             repeat(10){
-                println("\n Tirada $it J2, ha sacado: ${lista2.random()}")
+                println("\nTirada $it J2, ha sacado: ${lista2.random()}")
                 delay(1500)
                 y += lista2.random()
 
             }
-            println("\n J2 ha terminado")
+            println("\nJ2 ha terminado")
         }
 
     }
 
-    println("\n J1 ha sacado $x en total")
-    println(" J2 ha sacado $y en total")
+    println("\nJ1 ha sacado $x en total")
+    println("J2 ha sacado $y en total")
 
-    if (x>y){
-        println("\n HA GANADO EL JUGADOR 1")
-    } else {
-        println("\n HA GANADO EL JUGADOR 2")
-    }
-
-
+    if (x>y) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
 }
