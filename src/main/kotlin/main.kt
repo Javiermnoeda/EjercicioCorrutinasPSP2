82fe31c69e91f013ba324373eb33faa5c29967f4
Javier <jmnoeda2000@gmail.com>
2021-04-15 14:01:27 +0200

    Ejercicio Actualizado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -17,31 +17,31 @@ fun main() {
         this.launch {
             repeat(10) {
                 numSacado = random.nextInt(6)+1 //Genera Numeros del 1 al 6
-                println("\nTirada ${it + 1} de J1, ha sacado: $numSacado")
+                println("\nTirada ${it + 1} del Jugador 1, ha sacado: $numSacado")
                 listaJ1.add(numSacado)
                 delay(1000)
                 totalJ1 += numSacado
             }
             println("Todos los resultados del Jugador 1 son: $listaJ1")
-            println("\nJ1 ha terminado")
+            println("\nJugador 1 ha terminado")
         }
 
         this.launch {
             repeat(10) {
                 numSacado = random.nextInt(6)+1
-                println("\nTirada ${it + 1} de J2, ha sacado: $numSacado")
+                println("\nTirada ${it + 1} del Juagador 2, ha sacado: $numSacado")
                 listaJ2.add(numSacado)
                 delay(1500)
                 totalJ2 += numSacado
             }
             println("Todos los resultados del Jugador 2 son: $listaJ2")
-            println("\nJ2 ha terminado")
+            println("\nJugador 2 ha terminado")
         }
     }
 
-    println("\nJ1 ha sacado $totalJ1 en total")
-    println("J2 ha sacado $totalJ2 en total")
+    println("\nJugador 1 ha sacado $totalJ1 en total")
+    println("Jugador 2 ha sacado $totalJ2 en total")
 
-    if (totalJ1 > totalJ2) println("\nJ1 ha sacado el numero más alto") else println("\nJ2 ha sacado el numero más alto")
+    if (totalJ1 > totalJ2) println("\nJugador 1 ha sacado el numero más alto") else println("\nJugador 2 ha sacado el numero más alto")
 
 }
